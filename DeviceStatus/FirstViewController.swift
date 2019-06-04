28a2ae578ff4c1af880c8389cfe503f8acaa279c
Ollie <o.stowell@applicaster.com>
2019-06-04 18:53:48 +0100

    Initial Commit

---
+++ DeviceStatus/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DeviceStatus
+//
+//  Created by Ollie Stowell on 04/06/2019.
+//  Copyright © 2019 Oliver Stowell. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ DeviceStatus/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DeviceStatus
+//
+//  Created by Ollie Stowell on 04/06/2019.
+//  Copyright © 2019 Oliver Stowell. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
